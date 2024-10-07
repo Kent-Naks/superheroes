4e514b70c209c521287c2eb7354054b82e1ab369
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-10-07 23:43:39 +0300

    Seed the database with hero records.

--- seed.py
+++ seed.py
@@ -11,3 +11,23 @@ if __name__ == '__main__':
         Power.query.delete()  
         Hero.query.delete()  
         HeroPower.query.delete()
+        
+        # Seeding Heroes
+        print("Seeding heroes...")
+        # Create a list of Hero instances
+        heroes = [
+            Hero(name="Kamala Khan", super_name="Ms. Marvel"),
+            Hero(name="Doreen Green", super_name="Squirrel Girl"),
+            Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
+            Hero(name="Janet Van Dyne", super_name="The Wasp"),
+            Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
+            Hero(name="Carol Danvers", super_name="Captain Marvel"),
+            Hero(name="Jean Grey", super_name="Dark Phoenix"),
+            Hero(name="Ororo Munroe", super_name="Storm"),
+            Hero(name="Kitty Pryde", super_name="Shadowcat"),
+            Hero(name="Elektra Natchios", super_name="Elektra"),
+        ]
+        # Add all heroes to the session and commit them to the database
+        db.session.add_all(heroes)
+        db.session.commit()  # Commit the heroes to the database
+
